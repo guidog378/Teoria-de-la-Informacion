cd752deef93251422cf8c48b6ac258d511fca58c
lorenzo <lorenmarinucci@gmail.com>
2020-09-22 18:12:50 -0300

    Modulo simulacion

--- simulacion.py
+++ simulacion.py
@@ -1,5 +1,18 @@
+from random import random
+
 def simular_secuencia(simbolos, n):
     """En base a la longitud n de la secuencia, generar la misma en base
     a las probabilidades de cada símbolo"""
 
-    return ""
+    secuencia = ""
+
+    for i in range(n):
+        sumaIntervalo = 0
+        numeroAletorio = random()
+        for simbolo in simbolos.keys():
+            sumaIntervalo += simbolos[simbolo]
+            if (sumaIntervalo >= numeroAletorio): # de darse la condición, el numero aleatorio queda dentro del intervalo del símbolo
+                secuencia += simbolo
+                break
+    
+    return secuencia
